80043847bf2b1d607b5d8c88fe9d0bbf4ae93224
machimaria <machimaria0796@gmail.com>
2023-06-23 18:25:44 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,13 +11,14 @@ void print_diagonal(int n)
 	for (i = 0; i < n; i++)
 	{
 		if (n > 1)
-			for (j = 0; j < 1; j++)
+			for (j = 0; j < i; j++)
 			{
 				_putchar(' ');
 			}
 		_putchar('\\');
 		_putchar('\n');
 	}
+
 	if (n <= 0)
 	{
 		_putchar('\n');
