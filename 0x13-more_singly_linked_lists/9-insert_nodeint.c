cdf2a7c373fdff4c6b88498ba2b4a015467fc073
machimaria <machimaria0796@gmail.com>
2023-08-01 23:34:38 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,85 +1,42 @@
-include "lists.h"
-
-
+#include "lists.h"
 /**
-
- * insert_nodeint_at_index - inserts a new node in a linked list,
-
- * at a given position
-
- * @head: pointer to the first node in the list
-
- * @idx: index where the new node is added
-
+ * insert_nodeint_at_index - inserts a new node
+ * @head: pointer to the first node
+ * @idx: index
  * @n: data to insert in the new node
-
- *
-
  * Return: pointer to the new node, or NULL
-
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-
 {
+unsigned int h;
+listint_t *new;
+listint_t *tail = *head;
 
-        unsigned int i;
+new = malloc(sizeof(listint_t));
+if (!new || !head)
+return (NULL);
 
-        listint_t *new;
-
-        listint_t *temp = *head;
-
-
-        new = malloc(sizeof(listint_t));
-
-        if (!new || !head)
-
-                return (NULL);
-
-
-        new->n = n;
-
-        new->next = NULL;
-
-
-        if (idx == 0)
-
-        {
-
-                new->next = *head;
-
-                *head = new;
-
-                return (new);
-
-        }
-
-
-        for (i = 0; temp && i < idx; i++)
-
-        {
-
-                if (i == idx - 1)
-
-                {
-
-                        new->next = temp->next;
-
-                        temp->next = new;
-
-                        return (new);
-
-                }
-
-                else
-
-                        temp = temp->next;
-
-        }
-
-
-        return (NULL);
-
+new->n = n;
+new->next = NULL;
+if (idx == 0)
+{
+new->next = *head;
+*head = new;
+return (new);
 }
 
-
+for (h = 0; tail && h < idx; h++)
+{
+if (h == idx - 1)
+{
+new->next = tail->next;
+tail->next = new;
+return (new);
+}
+else
+{
+tail = tail->next;
+}
+}
+return (NULL);
+}
