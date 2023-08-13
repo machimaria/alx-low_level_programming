7561447c125246641a56fcdec61500565625ef0d
machimaria <machimaria0796@gmail.com>
2023-08-13 15:25:24 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fn = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fn = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
 
 	if (fn == -1)
 		return (-1);
