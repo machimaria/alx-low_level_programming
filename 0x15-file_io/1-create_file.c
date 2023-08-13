4ea31dc21904b0ca9db71d2f85126f7d41034313
machimaria <machimaria0796@gmail.com>
2023-08-13 15:09:31 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file
+ * @filename: the name of the file
+ * @text_content: content of the file
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fn;
+	int cletters;
+	int rw;
+
+	if (!filename)
+		return (-1);
+
+	fn = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fn == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (cletters = 0; text_content[cletters]; cletters++)
+
+	rw = write(fn, text_content, cletters);
+
+	if (rw == -1)
+		return (-1);
+	close(fn);
+
+	return (1);
+}
