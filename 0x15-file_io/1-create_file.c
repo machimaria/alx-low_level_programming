0f814d6a074c16eb6145b52365b8fee2bfbb0812
machimaria <machimaria0796@gmail.com>
2023-08-14 03:15:05 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,50 @@
 #include "main.h"
+
 /**
- * create_file - function that creates a file
- * @filename: the name of the file
- * @text_content: content of the file
- * Return: 1 or -1
+ * _strlen - compute the length of a NULL-terminated string
+ * @str: the string to measure
+ *
+ * Return: the length of str, or -1 if str is NULL
+ */
+ssize_t _strlen(const char *str)
+{
+	ssize_t len = 0;
+
+	if (!str)
+		return (-1);
+
+	while (*str++)
+		++len;
+
+	return (len);
+}
+
+/**
+ * create_file - create a file
+ * @filename: the name of the file to create
+ * @text_content: the data to write to filename
+ *
+ * Return: Upon success, return 1. Otherwise, return -1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fn;
-	int cletters;
-	int rw;
+	ssize_t b_written = 0;
+	int fd;
 
 	if (!filename)
 		return (-1);
 
-	fn = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-	if (fn == -1)
+	if (fd < 0)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
+	if (text_content)
+		b_written = write(fd, text_content, _strlen(text_content));
 
-	for (cletters = 0; text_content[cletters]; cletters++)
+	close(fd);
 
-	rw = write(fn, text_content, cletters);
-
-	if (rw == -1)
+	if (b_written < 0)
 		return (-1);
-	close(fn);
-
 	return (1);
 }
